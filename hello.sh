ef6c2966f0e1bfd699318ece82ecfae0b738d262
ajay131619 <pachhinela.ajay@gmail.com>
2024-10-12 21:56:19 +0530

    printing statements

--- hello.sh
+++ hello.sh
@@ -3,6 +3,7 @@
 # hi everyone! this is ajay 
 # here we are going to learn some basics for printing the statements 
 # we will use echo command for this file 
+# date:: Sat Oct 12 21:45:41 IST 2024
 
 echo "hi everyone!"
 echo "This is ajay"
