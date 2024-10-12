75da8fbc89356c0ba3ed5e623ea3362b857ebc10
ajay131619 <pachhinela.ajay@gmail.com>
2024-10-12 21:43:32 +0530

    printing statements

---
+++ hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# hi everyone! this is ajay 
+# here we are going to learn some basics for printing the statements 
+# we will use echo command for this file 
+
+echo "hi everyone!"
+echo "This is ajay"
