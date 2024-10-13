0db7e6585427a2ca82f8043c1896ca1342e965ef
ajay131619 <pachhinela.ajay@gmail.com>
2024-10-13 14:56:44 +0530

    mysql installation through shell script

--- sql-installation-script.sh
+++ sql-installation-script.sh
@@ -6,8 +6,8 @@
 userid=$(id -u)
 if[ $userid -ne 0 ]
    then
-   echo "Please run this script with root user access (sudo access)"
-  exit 1
+     echo "Please run this script with root user access (sudo access)"
+     exit1
 fi
 
 dnf install git -y
