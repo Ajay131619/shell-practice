a9f0f4ad248f8064878071519cb9c224bf4283c7
ajay131619 <pachhinela.ajay@gmail.com>
2024-10-13 14:24:57 +0530

    conditional statements

---
+++ conditional-statement.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#conditional statements 
+
+#greater than or less than for a given particular number
+
+number=$1
+if [ $number -gt 50 ]
+ then
+ echo "number is greater than 50"
+ else
+ echo "number is less than 50"
+fi
