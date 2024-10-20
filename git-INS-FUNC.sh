646c49fe1af4ca9a5225221429b1772b700caf6c
ajay131619 <pachhinela.ajay@gmail.com>
2024-10-20 17:36:18 +0530

    installation using functions

---
+++ git-INS-FUNC.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#date :: Thu, Oct 17, 2024  2:44:23 PM
+
+# this program is about the usage of functions
+
+#let us install git and github using functions
+
+userid=$(id -u)
+
+#user authentication function
+checkroot(){
+
+if [ $userid -ne 0 ]
+then 
+    echo " this program can only work with root user access"
+    exit 1
+fi
+}
+
+#installation function
+
+installation(){
+    if [ $? -ne 0 ]
+    then
+        echo"git is not there in your system. going to install it!"
+        dnf install git -y
+            if [ $? -ne 0 ]
+            then
+                echo"git is not installed successfully!!"
+                echo"PLEASE CHECK IT ONCE"
+                exit 1
+            else
+                echo"git is successfully!!!!"
+                exit 1
+            fi
+    else
+        echo"git is already installed in your system"
+        exit 1
+    fi
+}
+
+checkroot()
+
+dnf list installed git
+
+installation()
